4b8ea14fe38a74b3ed4d3e91d95a7008f009b887
Colin Gourlay <gourlay.colin@abc.net.au>
2020-10-20 13:22:32 +1000

    Remove 1px border from hex grid (all data needed to be reduced by 1px in x and y dimensions)

--- src/components/Tilegram/index.tsx
+++ src/components/Tilegram/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Allocation, Allocations, Wall, Walls, GroupID, GROUPS, StateID, STATES } from '../../constants';
 import { getGroupIDsForStateID, getGroupIDForStateIDAndDelegateIndex } from '../../utils';
-import { COUNTRY_PATHS, STATES_DELEGATE_HEXES, STATES_LABELS, STATES_SHAPES, LANDMASS_PROPS } from './data';
+import { COUNTRY_PATHS, STATES_DELEGATE_HEXES, STATES_LABELS, STATES_SHAPES, HEXGRID_PROPS } from './data';
 import styles from './styles.scss';
 
 // inner stroke methods: https://stackoverflow.com/questions/7241393/can-you-control-how-an-svgs-stroke-width-is-drawn
@@ -43,8 +43,8 @@ const Tilegram: React.FC<TilegramProps> = props => {
     }
   };
 
-  const svgWidth = LANDMASS_PROPS.width + 2 * LANDMASS_PROPS.margin;
-  const svgHeight = LANDMASS_PROPS.height + 2 * LANDMASS_PROPS.margin;
+  const svgWidth = HEXGRID_PROPS.width + 2 * HEXGRID_PROPS.margin;
+  const svgHeight = HEXGRID_PROPS.height + 2 * HEXGRID_PROPS.margin;
 
   const svgAttrs = {
     width: svgWidth,
@@ -55,7 +55,7 @@ const Tilegram: React.FC<TilegramProps> = props => {
   return (
     <div className={`${styles.root}${isInteractive ? ` ${styles.isInteractive}` : ''}`} data-tappable={tappableLayer}>
       <svg className={styles.svg} {...svgAttrs}>
-        <g transform={`translate(${LANDMASS_PROPS.margin} ${LANDMASS_PROPS.margin})`}>
+        <g transform={`translate(${HEXGRID_PROPS.margin} ${HEXGRID_PROPS.margin})`}>
           <g className={styles.country}>
             {COUNTRY_PATHS.map((d, index) => (
               <path key={`${d}_${index}`} d={d}></path>
