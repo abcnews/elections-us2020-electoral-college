ed6dc6e210b1f822c8a30d3c0e39a1084fc0ef0c
Colin Gourlay <gourlay.colin@abc.net.au>
2020-10-21 10:11:43 +1000

    Convert Totals text to basic bars representation

--- src/components/Totals/index.tsx
+++ src/components/Totals/index.tsx
@@ -7,20 +7,48 @@ export type TotalsProps = {
   allocations?: Allocations;
 };
 
+const MAX_VOTES = 538;
+const WIN_VOTES = Math.ceil(MAX_VOTES / 2);
+
 const Totals: React.FC<TotalsProps> = props => {
   const { allocations } = props;
   const voteCounts = useMemo(() => getVoteCountsForAllocations(allocations || {}), [allocations]);
 
   return (
     <div className={styles.root}>
-      {[
-        `Dem.: ${voteCounts[Allocation.Dem]}`,
-        `Likely Dem.: ${voteCounts[Allocation.LikelyDem]}`,
-        `Tossup: ${voteCounts[Allocation.Tossup]}`,
-        `Likely GOP: ${voteCounts[Allocation.LikelyGOP]}`,
-        `GOP: ${voteCounts[Allocation.GOP]}`,
-        `Unallocated: ${voteCounts[Allocation.None]}`
-      ].join('; ')}
+      <div className={styles.track}>
+        <div
+          className={styles.bar}
+          title={`Likely Dem.: ${voteCounts[Allocation.LikelyDem]}`}
+          data-allocation={Allocation.LikelyDem}
+          style={{
+            transform: `scale(${(voteCounts[Allocation.Dem] + voteCounts[Allocation.LikelyDem]) / MAX_VOTES}, 1)`
+          }}
+        ></div>
+        <div
+          className={styles.bar}
+          title={`Likely GOP: ${voteCounts[Allocation.LikelyGOP]}`}
+          data-allocation={Allocation.LikelyGOP}
+          style={{
+            transform: `scale(${(voteCounts[Allocation.GOP] + voteCounts[Allocation.LikelyGOP]) / MAX_VOTES}, 1)`
+          }}
+        ></div>
+        <div
+          className={styles.bar}
+          title={`Dem.: ${voteCounts[Allocation.Dem]}`}
+          data-allocation={Allocation.Dem}
+          style={{ transform: `scale(${voteCounts[Allocation.Dem] / MAX_VOTES}, 1)` }}
+        ></div>
+        <div
+          className={styles.bar}
+          title={`GOP: ${voteCounts[Allocation.GOP]}`}
+          data-allocation={Allocation.GOP}
+          style={{ transform: `scale(${voteCounts[Allocation.GOP] / MAX_VOTES}, 1)` }}
+        ></div>
+        <div className={styles.win}>
+          <div className={styles.winLabel}>270 to win</div>
+        </div>
+      </div>
     </div>
   );
 };
