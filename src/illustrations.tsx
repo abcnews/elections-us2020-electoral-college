01d8d001cce87bee6e5e8e0a71c9a22dc64729a3
Colin Gourlay <gourlay.colin@abc.net.au>
2020-11-03 11:56:03 +1000

    Make Illustration markers copiable

--- src/illustrations.tsx
+++ src/illustrations.tsx
@@ -4,23 +4,28 @@ import React from 'react';
 import { render } from 'react-dom';
 import Illustration, { IllustrationName } from './components/Illustration';
 
-console.log(Object.values(IllustrationName));
+const MarkerText = ({ text }) => (
+  <button onClick={() => navigator.clipboard.writeText(text)} style={{ cursor: 'copy', padding: '0.25rem 0.5rem' }}>
+    <code>{text}</code>
+  </button>
+);
 
-whenDOMReady.then(() =>
-  render(
-    <article>
-      <h1>Illustrations</h1>
-      <div>
-        {Object.values(IllustrationName).map(name => (
-          <div key={name}>
-            <h2>
-              <code>{`#ecillustrationNAME${name}`}</code>
-            </h2>
+const Article = () => (
+  <article style={{ fontFamily: 'ABCSans' }}>
+    <h1>Illustrations</h1>
+    <div>
+      {Object.values(IllustrationName).map((name, index) => (
+        <div key={name} style={{ margin: '0 0 2rem' }}>
+          <div style={{ margin: '0 0 1rem', maxWidth: '20rem' }}>
             <Illustration name={name} />
           </div>
-        ))}
-      </div>
-    </article>,
-    selectMounts('ecillustrations')[0]
-  )
+          <MarkerText text={`#ecillustrationNAME${name}`}></MarkerText>
+          {index === 0 && ' or '}
+          {index === 0 && <MarkerText text="#ecillustration"></MarkerText>}
+        </div>
+      ))}
+    </div>
+  </article>
 );
+
+whenDOMReady.then(() => render(<Article />, selectMounts('ecillustrations')[0]));
