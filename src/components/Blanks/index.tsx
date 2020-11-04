b931e191bf43e68337552c813d8a973c71e984df
Colin Gourlay <gourlay.colin@abc.net.au>
2020-11-04 19:54:45 +1000

    Added a hint

--- src/components/Blanks/index.tsx
+++ src/components/Blanks/index.tsx
@@ -26,6 +26,7 @@ const defaultGraphicProps = {
 const Blanks: React.FC<BlanksProps> = ({ isLive, initialGraphicProps }) => {
   const [fixedGraphicProps, setFixedGraphicProps] = useState<GraphicProps>(defaultGraphicProps);
   const [audienceAllocations, setAudienceAllocations] = useState<Allocations>({});
+  const [hasInteracted, setHasInteracted] = useState(false);
 
   const onTapGroup = (groupID: string) => {
     if (!fixedGraphicProps || !fixedGraphicProps.allocations) {
@@ -37,6 +38,10 @@ const Blanks: React.FC<BlanksProps> = ({ isLive, initialGraphicProps }) => {
       fixedGraphicProps.allocations[groupID] === Allocation.GOP
     ) {
       return console.error(`${groupID} is already called`);
+    }
+
+    if (!hasInteracted) {
+      setHasInteracted(true);
     }
 
     const allocation = audienceAllocations[groupID] || Allocation.None;
@@ -139,10 +144,20 @@ const Blanks: React.FC<BlanksProps> = ({ isLive, initialGraphicProps }) => {
   }, []);
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} data-has-interacted={hasInteracted ? '' : undefined}>
       <div className={styles.graphic}>
         <Graphic {...graphicProps} />
       </div>
+      {hasInteracted ? null : (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 255" width="80" height="104" className={styles.hint}>
+          <g>
+            <path
+              d="M49.5 140.2v-2.5V24.4c0-7.1 4.3-13.1 11-15.3 6.5-2.2 13.7.1 17.9 5.7 2.1 2.8 3.1 6.1 3.1 9.6V78.7c5.4-6.2 11.7-7.9 18.9-4.7 7.1 3.1 9.8 9.1 9.4 17.2 5.1-5.4 11.1-6.8 17.6-5.1 6.8 1.8 10.6 6.4 11.7 13.6.7-.4 1.2-.8 1.7-1.1 5.3-3.5 10.7-3.8 16.2-.8s8.4 7.8 8.4 14.2v74c0 27.2-19.6 51-46.3 56.3-25.3 5-49.9-6.4-62.6-28.9-16.6-29.6-33.4-59.1-50.2-88.6-.9-1.5-.7-2.3.5-3.5 9.5-9.3 23.2-8.1 30.9 2.8 3.5 4.9 7 9.8 10.5 14.8.4.1.7.5 1.3 1.3z"
+              fill="#b3b3b3"
+            />
+          </g>
+        </svg>
+      )}
     </div>
   );
 };
