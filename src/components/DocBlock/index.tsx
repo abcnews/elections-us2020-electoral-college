83fa8de55d9601080cb0361efd27cd6b31fd13fb
Colin Gourlay <gourlay.colin@abc.net.au>
2020-11-20 10:24:39 +1000

    Refactor DocBlock component to use loadScrollyteller instead of its own scrolyteller/mark parsing, create a renderPreview prop for rendering the scrollyteller, as well as other props to make the component more generic and enable dependency injection for preprocessing Google Doc text, tweaking the scrollyteller definition and adding a custom fallback images button.

--- src/components/DocBlock/index.tsx
+++ src/components/DocBlock/index.tsx
@@ -1,47 +1,43 @@
-import * as acto from '@abcnews/alternating-case-to-object';
-import { getMountValue, isMount } from '@abcnews/mount-utils';
 import React, { useRef, useState } from 'react';
-import { render } from 'react-dom';
-import type { PanelDefinition } from '@abcnews/scrollyteller';
-import { applyColourToPanels } from '../../panels';
-import { decodeAllocations, decodeFocuses, graphicPropsToAlternatingCase, urlQueryToGraphicProps } from '../../utils';
-import Live from '../Live';
-import Block from '../Block';
-import type { GraphicProps } from '../Graphic';
+import { PanelDefinition, ScrollytellerDefinition } from '@abcnews/scrollyteller';
+import { loadScrollyteller } from '@abcnews/scrollyteller';
 import styles from './styles.scss';
-import type { OdysseySchedulerClient, OdysseySchedulerSubscriber } from '../..';
 
-const subscribers = new Set<OdysseySchedulerSubscriber>();
+const URL_LOCALSTORAGE_KEY = 'last-successfully-loaded-google-doc-url';
 
-const notifySubscribers = (event: Event) => {
-  const client: OdysseySchedulerClient = { hasChanged: event.type === 'resize', fixedHeight: window.innerHeight };
+const IDENTITY = x => x;
 
-  Array.from(subscribers).map(fn => fn(client));
+type DocBlockProps<T> = {
+  loadScrollytellerArgs?: {
+    name?: string;
+    className?: string;
+    markerName?: string;
+  };
+  preprocessCoreEl?: (el: Element) => Element;
+  postprocessScrollytellerDefinition?: (
+    scrollytellerDefinition: ScrollytellerDefinition<T>
+  ) => ScrollytellerDefinition<T>;
+  renderPreview: (scrollytellerDefinition: ScrollytellerDefinition<T>) => React.ReactElement;
+  renderFallbackImagesButton?: (scrollytellerDefinition: ScrollytellerDefinition<T>) => React.ReactElement;
 };
 
-window.addEventListener('scroll', notifySubscribers);
-window.addEventListener('resize', notifySubscribers);
-
-window.__ODYSSEY__ = {
-  scheduler: {
-    subscribe: fn => subscribers.add(fn),
-    unsubscribe: fn => subscribers.delete(fn)
+function DocBlock<T>({
+  loadScrollytellerArgs = {
+    className: 'u-full',
+    markerName: 'mark'
   },
-  utils: {
-    dom: {
-      detach: () => {}
-    }
-  }
-};
-
-const URL_LOCALSTORAGE_KEY = 'ecdburl';
-
-const DocBlock: React.FC = () => {
+  preprocessCoreEl,
+  postprocessScrollytellerDefinition,
+  renderPreview,
+  renderFallbackImagesButton
+}: DocBlockProps<T>): React.ReactElement {
+  const { name, className, markerName } = loadScrollytellerArgs;
   const inputRef = useRef<HTMLInputElement>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [coreText, setCoreText] = useState<string>();
   const [coreHTML, setCoreHTML] = useState<string>();
-  const [panels, setPanels] = useState<PanelDefinition<GraphicProps>[]>();
+  const [preview, setPreview] = useState<React.ReactElement>();
+  const [fallbackImagesButton, setFallbackImagesButton] = useState<React.ReactElement>();
 
   const load = () => {
     const url = inputRef.current?.value;
@@ -74,88 +70,95 @@ const DocBlock: React.FC = () => {
           el.removeAttribute('id');
         });
 
-        const nodes = Array.from(body.children).map(el => {
+        const coreEls: Element[] = Array.from(body.children).map(preprocessCoreEl ? preprocessCoreEl : IDENTITY);
+
+        const coreText = coreEls.reduce<string>((memo, el) => {
           const text = String(el.textContent).trim();
-          const linkEl: HTMLAnchorElement | null = el.querySelector('a[href]');
-          const urlQuery = linkEl && (new URL(linkEl.href).searchParams.get('q') || '').split('?')[1];
-          const markerMatch = text.match(/#(scrollyteller|mark)/);
-
-          if (urlQuery && markerMatch) {
-            const markerPrefix = markerMatch[1] === 'scrollyteller' ? 'scrollytellerNAMEecblock' : 'mark';
-            const graphicProps = urlQueryToGraphicProps('?' + urlQuery);
-            const pEl = document.createElement('p');
-
-            pEl.textContent = `#${markerPrefix}${graphicPropsToAlternatingCase(graphicProps)}`;
-
-            return pEl;
-          }
-
-          return el;
-        });
-
-        const coreText = nodes.reduce((memo, node) => {
-          const text = String(node.textContent).trim();
 
           memo = `${memo}\n${text ? `\n${text}` : ''}`;
 
           return memo;
         }, '');
 
-        const coreHTML = nodes.reduce((memo, node) => {
-          const text = String(node.textContent).trim();
-          const html = node.outerHTML;
+        const coreHTML = coreEls.reduce<string>((memo, el) => {
+          const text = String(el.textContent).trim();
+          const html = el.outerHTML;
 
           memo = `${memo}${text ? `${html}` : ''}`;
 
           return memo;
         }, '');
 
-        const panels = loadPanels(
-          nodes.reduce<{ isRemoving: boolean; config: LoadPanelsConfig }>(
-            (memo, el) => {
-              const { config } = memo;
-              const text = String(el.textContent).trim();
+        const { scrollytellingEls } = coreEls.reduce<{
+          hasBegun: boolean;
+          hasEnded: boolean;
+          isRemoving: boolean;
+          scrollytellingEls: Element[];
+        }>(
+          (memo, el) => {
+            if (memo.hasEnded) {
+              return memo;
+            }
 
-              if (text.indexOf('#remove') === 0) {
-                memo.isRemoving = true;
-              } else if (text.indexOf('#endremove') === 0) {
-                memo.isRemoving = false;
-              } else if (text.indexOf('#eclive') === 0) {
-                const { state, test, hide } = acto(text.slice(1));
+            function textToMountEl(text) {
+              const mountEl = document.createElement('div');
 
-                if (typeof hide !== 'boolean' || !hide) {
-                  render(<Live stateCode={state.toUpperCase()} test={test} />, el);
-                  config.nodes.push(el);
-                }
-              } else if (text.indexOf('#scrollyteller') === 0 && !config.initialProps) {
-                config.initialProps = acto(text.slice(1));
-              } else if (!config.initialProps || config.name || memo.isRemoving || text === '') {
-                // skip
+              mountEl.setAttribute('data-mount', '');
+              mountEl.setAttribute('id', text.slice(1));
+
+              return mountEl;
+            }
+
+            const text = String(el.textContent).trim();
+
+            if (text.indexOf('#remove') === 0) {
+              memo.isRemoving = true;
+            } else if (text.indexOf('#endremove') === 0) {
+              memo.isRemoving = false;
+            } else if (text.indexOf('#') === 0) {
+              if (text.indexOf(`#scrollyteller${name ? `NAME${name}` : ''}`) === 0 && !memo.hasBegun) {
+                memo.hasBegun = true;
               } else if (text.indexOf('#endscrollyteller') === 0) {
-                config.name = 'mark';
-              } else if (text.indexOf('#mark') === 0) {
-                const mountEl = document.createElement('div');
-
-                mountEl.setAttribute('data-mount', '');
-                mountEl.setAttribute('id', text.slice(1));
-                config.nodes.push(mountEl);
-              } else {
-                config.nodes.push(el);
+                memo.hasEnded = true;
               }
 
-              return memo;
-            },
-            {
-              config: {
-                nodes: []
-              },
-              isRemoving: false
+              memo.scrollytellingEls.push(textToMountEl(text));
+            } else if (!memo.hasBegun || memo.isRemoving || text === '') {
+              // skip
+            } else {
+              memo.scrollytellingEls.push(el);
             }
-          ).config
+
+            return memo;
+          },
+          {
+            hasBegun: false,
+            hasEnded: false,
+            isRemoving: false,
+            scrollytellingEls: []
+          }
         );
 
-        applyColourToPanels(panels);
-        setPanels(panels);
+        const tempContainerEl = document.createElement('div');
+
+        tempContainerEl.className = styles.tempContainerEl;
+        scrollytellingEls.forEach(scrollytellingEl => tempContainerEl.appendChild(scrollytellingEl));
+        document.body.appendChild(tempContainerEl);
+
+        let scrollytellerDefinition: ScrollytellerDefinition<T> = loadScrollyteller(name, className, markerName);
+
+        document.body.removeChild(tempContainerEl);
+
+        if (postprocessScrollytellerDefinition) {
+          scrollytellerDefinition = postprocessScrollytellerDefinition(scrollytellerDefinition);
+        }
+
+        setPreview(renderPreview(scrollytellerDefinition));
+
+        if (renderFallbackImagesButton) {
+          setFallbackImagesButton(renderFallbackImagesButton(scrollytellerDefinition));
+        }
+
         setCoreText(coreText);
         setCoreHTML(coreHTML);
         setIsLoading(false);
@@ -165,7 +168,7 @@ const DocBlock: React.FC = () => {
 
   return (
     <div className={styles.root} data-is-loading={isLoading ? '' : undefined}>
-      <div className={styles.block}>{panels && <Block panels={panels} />}</div>
+      <div className={styles.preview}>{preview}</div>
       <div className={styles.controls}>
         <div className={styles.row}>
           <input
@@ -200,85 +203,12 @@ const DocBlock: React.FC = () => {
             >
               Core Text
             </button>
-            <button
-              onClick={event => {
-                const buttonEl = event.target;
-
-                import(/* webpackChunkName: "fallbacks" */ '../../fallbacks').then(module =>
-                  module.default(buttonEl, panels)
-                );
-              }}
-            >
-              Fallback Images
-            </button>
+            {fallbackImagesButton}
           </div>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default DocBlock;
-
-const piecemeal = Symbol('piecemeal');
-
-type LoadPanelsConfig = { nodes: Node[]; initialProps?: GraphicProps; name?: string };
-
-const loadPanels = ({ nodes, initialProps, name }: LoadPanelsConfig): PanelDefinition<GraphicProps>[] => {
-  name = name || 'mark';
-
-  console.debug({ nodes, initialProps, name });
-
-  let panels: PanelDefinition<GraphicProps>[] = [];
-  let nextProps: GraphicProps = initialProps || {};
-  let nextNodes: Node[] = [];
-
-  // Commit the current nodes to a marker
-  function pushPanel() {
-    if (nextNodes.length === 0) return;
-
-    panels.push({
-      data: nextProps,
-      nodes: nextNodes
-    });
-    nextNodes = [];
-  }
-
-  // Check the section nodes for panels and marker content
-  nodes.forEach((node: Node, index: number) => {
-    if (isMount(node, name)) {
-      // Found a new marker so we should commit the last one
-      pushPanel();
-
-      // If marker has no config then just use the previous config
-      let ac: string = getMountValue(node, name);
-
-      if (ac) {
-        const props = acto(ac);
-
-        props.allocations = decodeAllocations((props.allocations as string) || '');
-        props.focuses = decodeFocuses((props.focuses as string) || '');
-
-        nextProps = props as GraphicProps;
-      } else {
-        // Empty marks should stop the piecemeal flow
-        nextProps[piecemeal] = false;
-      }
-    } else {
-      // Any other nodes just get grouped for the next marker
-      nextNodes.push(node);
-    }
-
-    // Any trailing nodes just get added as a last marker
-    if (index === nodes.length - 1) {
-      pushPanel();
-    }
-
-    // If piecemeal is on/true then each node has its own box
-    if (nextProps[piecemeal]) {
-      pushPanel();
-    }
-  });
-
-  return panels;
-};
--- src/doc-block.tsx
+++ src/doc-block.tsx
@@ -1,7 +1,105 @@
+import * as acto from '@abcnews/alternating-case-to-object';
 import { whenDOMReady } from '@abcnews/env-utils';
-import { selectMounts } from '@abcnews/mount-utils';
+import { isMount, selectMounts } from '@abcnews/mount-utils';
+import type { PanelDefinition } from '@abcnews/scrollyteller';
 import React from 'react';
 import { render } from 'react-dom';
+import Block from './components/Block';
 import DocBlock from './components/DocBlock';
+import type { GraphicProps, PossiblyEncodedGraphicProps } from './components/Graphic';
+import Live from './components/Live';
+import { applyColourToPanels } from './panels';
+import { decodeAllocations, decodeFocuses, graphicPropsToAlternatingCase, urlQueryToGraphicProps } from './utils';
+import type { OdysseySchedulerClient, OdysseySchedulerSubscriber } from '.';
 
-whenDOMReady.then(() => render(<DocBlock />, selectMounts('ecdb')[0]));
+const subscribers = new Set<OdysseySchedulerSubscriber>();
+
+const notifySubscribers = (event: Event) => {
+  const client: OdysseySchedulerClient = { hasChanged: event.type === 'resize', fixedHeight: window.innerHeight };
+
+  Array.from(subscribers).map(fn => fn(client));
+};
+
+window.addEventListener('scroll', notifySubscribers);
+window.addEventListener('resize', notifySubscribers);
+
+window.__ODYSSEY__ = {
+  scheduler: {
+    subscribe: fn => subscribers.add(fn),
+    unsubscribe: fn => subscribers.delete(fn)
+  },
+  utils: {
+    dom: {
+      detach: () => {}
+    }
+  }
+};
+
+const LOAD_SCROLLYTELLER_ARGS = { name: 'ecblock', markerName: 'mark' };
+const MARKER_WITH_PROPS_PATTERN = /#(scrollyteller|mark)/;
+
+whenDOMReady.then(() =>
+  render(
+    <DocBlock<PossiblyEncodedGraphicProps>
+      preprocessCoreEl={el => {
+        const text = String(el.textContent).trim();
+        const linkEl: HTMLAnchorElement | null = el.querySelector('a[href]');
+        const urlQuery = linkEl && (new URL(linkEl.href).searchParams.get('q') || '').split('?')[1];
+        const markerMatch = text.match(MARKER_WITH_PROPS_PATTERN);
+
+        if (urlQuery && markerMatch) {
+          const markerPrefix =
+            markerMatch[1] === 'scrollyteller'
+              ? `scrollytellerNAME${LOAD_SCROLLYTELLER_ARGS.name}`
+              : LOAD_SCROLLYTELLER_ARGS.markerName;
+          const graphicProps = urlQueryToGraphicProps('?' + urlQuery);
+          const pEl = document.createElement('p');
+
+          pEl.textContent = `#${markerPrefix}${graphicPropsToAlternatingCase(graphicProps)}`;
+
+          return pEl;
+        }
+
+        return el;
+      }}
+      loadScrollytellerArgs={LOAD_SCROLLYTELLER_ARGS}
+      postprocessScrollytellerDefinition={scrollytellerDefinition => {
+        scrollytellerDefinition.panels.forEach(({ data, nodes }) => {
+          data.allocations = decodeAllocations((data.allocations as string) || '');
+          data.focuses = decodeFocuses((data.focuses as string) || '');
+
+          nodes.forEach(el => {
+            if (isMount(el, 'eclive')) {
+              const { state, test, hide } = acto(el.id || '');
+
+              if (typeof hide !== 'boolean' || !hide) {
+                render(<Live stateCode={state.toUpperCase()} test={test} />, el);
+              }
+            }
+          });
+        });
+
+        applyColourToPanels(scrollytellerDefinition.panels);
+
+        return scrollytellerDefinition;
+      }}
+      renderPreview={({ panels }) => <Block panels={panels as PanelDefinition<GraphicProps>[]} />}
+      renderFallbackImagesButton={({ panels }) => (
+        <button
+          onClick={event => {
+            const buttonEl = event.target;
+
+            if (panels) {
+              import(/* webpackChunkName: "fallbacks" */ './fallbacks').then(module =>
+                module.default(buttonEl, panels as PanelDefinition<GraphicProps>[])
+              );
+            }
+          }}
+        >
+          Fallback Images
+        </button>
+      )}
+    />,
+    selectMounts('ecdb')[0]
+  )
+);
